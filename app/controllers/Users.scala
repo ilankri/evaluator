eac882be6bf31eff7e50ed5a783d860f993a67d9
Idir Lankri <lankri.idir@gmail.com>
2018-01-05 21:21:01 +0100

    controllers: Implement Users.home

--- app/controllers/Users.scala
+++ app/controllers/Users.scala
@@ -1,7 +1,7 @@
 package controllers
 
 class Users(cc: UserControllerComponents) extends UserAbstractController(cc) {
-  def home = TODO
+  def home = userAction { request => Ok(views.html.home(request.user)) }
 
   def signout = userAction { request =>
     Redirect(routes.HomeController.signinPage).withSession(
