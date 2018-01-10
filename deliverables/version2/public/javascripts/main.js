d790546d7a3338721cf2fe6b0a3354547d2426b3
Idir Lankri <lankri.idir@gmail.com>
2018-01-10 23:45:25 +0100

    Improve the user interface

--- deliverables/version2/public/javascripts/main.js
+++ deliverables/version2/public/javascripts/main.js
@@ -1,19 +1,36 @@
-function sendRequest(method, url) {
+function sendRequest(method, url, onSuccess) {
     const request = new XMLHttpRequest();
 
     request.open(method, url, true);
     request.send(null);
+
+    /* On success, update the view accordingly.  */
+    request.onreadystatechange = function() {
+        if(request.readyState == XMLHttpRequest.DONE && request.status == 200) {
+            onSuccess();
+        }
+    };
 }
 
 
-function sendTaskRequest(method, taskId, userId) {
-    sendRequest(method, "/tasks/" + taskId + "/users/" + userId);
+function sendTaskRequest(method, taskId, userId, onSuccess) {
+    sendRequest(method, "/tasks/" + taskId + "/users/" + userId, onSuccess);
+}
+
+function removeElementById(id) {
+    const elem = document.getElementById(id);
+
+    elem.parentNode.removeChild(elem);
 }
 
 function registerTaskRequest(taskId, userId) {
-    sendTaskRequest("POST", taskId, userId);
+    sendTaskRequest("POST", taskId, userId, function() {
+        removeElementById("button" + taskId);
+    });
 }
 
 function unregisterTaskRequest(taskId, userId) {
-    sendTaskRequest("DELETE", taskId, userId);
+    sendTaskRequest("DELETE", taskId, userId, function() {
+        removeElementById("task" + taskId);
+    });
 }
