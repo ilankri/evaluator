36fa9973aa44a9d944215dc64a139b9101d695df
Idir Lankri <lankri.idir@gmail.com>
2018-01-16 22:16:48 +0100

    Use directly functions instead of classes for action functions

--- deliverables/version2/app/controllers/UserRequest.scala
+++ deliverables/version2/app/controllers/UserRequest.scala
@@ -4,8 +4,6 @@ import concurrent._
 
 import play.api.mvc._
 
-import db._
-
 class UserRequest[A](
     request: MessagesRequest[A],
     val user: models.User)
@@ -21,53 +19,50 @@ class EvaluatorRequest[A](
     override val user: models.Evaluator)
   extends UserRequest[A](request, user)
 
-class UserAction(
-    db: Db,
-    userIdKey: String,
-    resultOnUnauthorized: Result,
-    override val executionContext: ExecutionContext)
-  extends ActionRefiner[MessagesRequest, UserRequest] {
-  override def refine[A](request: MessagesRequest[A]) = Future.successful {
-    val resOnUnauthorized: Either[Result, UserRequest[A]] =
-      Left(resultOnUnauthorized)
-    request.session.get(userIdKey).fold(resOnUnauthorized)(id =>
-      db.readUser(id.toLong).fold(resOnUnauthorized)(user =>
-        Right(new UserRequest(request, user))))
-  }
-}
-
-class WorkerAction(
-    resultOnUnauthorized: Result,
-    override val executionContext: ExecutionContext)
-  extends ActionRefiner[UserRequest, WorkerRequest] {
-  override def refine[A](request: UserRequest[A]) = Future.successful {
-    request.user match {
-      case worker: models.Worker =>
-        Right(new WorkerRequest(request, worker))
-      case _ => Left(resultOnUnauthorized)
-    }
-  }
-}
-
-class EvaluatorAction(
-    resultOnUnauthorized: Result,
-    override val executionContext: ExecutionContext)
-  extends ActionRefiner[UserRequest, EvaluatorRequest] {
-
-  override def refine[A](request: UserRequest[A]) = Future.successful {
-    request.user match {
-      case evaluator: models.Evaluator =>
-        Right(new EvaluatorRequest(request, evaluator))
-      case _ => Left(resultOnUnauthorized)
-    }
-  }
-}
-
 abstract class UserAbstractController(cc: AppControllerComponents)
   extends MessagesAbstractController(cc) {
-  private def permissionCheckAction(id: Long, ec: ExecutionContext) =
+  private def withUserAction =
+    new ActionRefiner[MessagesRequest, UserRequest] {
+      override def executionContext = cc.executionContext
+
+      override def refine[A](request: MessagesRequest[A]) = Future.successful {
+        val resOnUnauthorized: Either[Result, UserRequest[A]] =
+          Left(Redirect(routes.AppController.signinForm))
+        request.session.get(cc.userIdKey).fold(resOnUnauthorized)(id =>
+          cc.db.readUser(id.toLong).fold(resOnUnauthorized)(user =>
+            Right(new UserRequest(request, user))))
+      }
+    }
+
+  private def asWorkerAction =
+    new ActionRefiner[UserRequest, WorkerRequest] {
+      override def executionContext = cc.executionContext
+
+      override def refine[A](request: UserRequest[A]) = Future.successful {
+        request.user match {
+          case worker: models.Worker =>
+            Right(new WorkerRequest(request, worker))
+          case _ => Left(AppResults.unauthorized)
+        }
+      }
+    }
+
+  private def asEvaluatorAction =
+    new ActionRefiner[UserRequest, EvaluatorRequest] {
+      override def executionContext = cc.executionContext
+
+      override def refine[A](request: UserRequest[A]) = Future.successful {
+        request.user match {
+          case evaluator: models.Evaluator =>
+            Right(new EvaluatorRequest(request, evaluator))
+          case _ => Left(AppResults.unauthorized)
+        }
+      }
+    }
+
+  private def permissionCheckAction(id: Long) =
     new ActionFilter[UserRequest] {
-      override def executionContext = ec
+      override def executionContext = cc.executionContext
 
       override def filter[A](request: UserRequest[A]) = Future.successful {
         if (request.user.id != id) Some(AppResults.unauthorized) else
@@ -75,26 +70,13 @@ abstract class UserAbstractController(cc: AppControllerComponents)
       }
     }
 
-  private def asWorkerAction =
-    new WorkerAction(AppResults.unauthorized, cc.executionContext)
-
-  private def asEvaluatorAction =
-    new EvaluatorAction(AppResults.unauthorized, cc.executionContext)
-
-  private def userAction =
-    Action andThen new UserAction(
-      cc.db,
-      cc.userIdKey,
-      Redirect(routes.AppController.signinForm),
-      cc.executionContext
-    )
+  private def userAction = Action andThen withUserAction
 
   def workerAction = userAction andThen asWorkerAction
 
   def evaluatorAction = userAction andThen asEvaluatorAction
 
-  def authUserAction(id: Long) =
-    userAction andThen permissionCheckAction(id, cc.executionContext)
+  def authUserAction(id: Long) = userAction andThen permissionCheckAction(id)
 
   def authWorkerAction(id: Long) = authUserAction(id) andThen asWorkerAction
 
